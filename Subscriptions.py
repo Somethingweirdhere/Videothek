d2c9b2013d619ce323575c834a623c30e954603f
root <root@gregord.vsos.ethz.ch>
2019-12-20 00:54:57 +0100

    Fixed error

--- Subscriptions.py
+++ Subscriptions.py
@@ -4,40 +4,44 @@ import json
 
 subscriptions = {}
 
+
 def process(lecture, user, subscribing):
-	if lecture in subscriptions:
-		if subscribing == 1 and not user in subscriptions[lecture]["users"]:
-			subscriptions[lecture]["users"].append(user)
-		if subscribing == 0 and user in subscriptions[lecture]["users"]:
-			subscriptions[lecture]["users"].remove(user)
-	elif subscribing == 1:
-		numberOfVideos = len(getVideoList(lecture))
-		subscriptions[lecture] = {"count": numberOfVideos, "users": [user]}
-	save()
+    if lecture in subscriptions:
+        if subscribing == 1 and not user in subscriptions[lecture]["users"]:
+            subscriptions[lecture]["users"].append(user)
+        if subscribing == 0 and user in subscriptions[lecture]["users"]:
+            subscriptions[lecture]["users"].remove(user)
+    elif subscribing == 1:
+        numberOfVideos = len(getVideoList(lecture))
+        subscriptions[lecture] = {"count": numberOfVideos, "users": [user]}
+    save()
 
 
 def load():
-	with open('subscriptions') as json_file:
-		global subscriptions
-		subscriptions = json.load(json_file)
+    with open('subscriptions') as json_file:
+        global subscriptions
+        subscriptions = json.load(json_file)
+
 
 def save():
-	with open('subscriptions', 'w') as json_file:
-		json.dump(subscriptions, json_file)
+    with open('subscriptions', 'w') as json_file:
+        json.dump(subscriptions, json_file)
+
 
 def checkChanges():
-	changes = []
-	global subscriptions
+    changes = []
+    global subscriptions
 
-	sbscpy = subscriptions
+    sbscpy = subscriptions
 
-	for lecture in sbscpy:
-		videos = getVideoList(lecture)
-		updatedCount = len(videos)
-		if sbscpy[lecture]["count"] < updatedCount:
-			sbscpy[lecture]["count"] = updatedCount
+    for lecture in sbscpy:
+        videos = getVideoList(lecture)
+        updatedCount = len(videos)
+        if sbscpy[lecture]["count"] < updatedCount:
+            sbscpy[lecture]["count"] = updatedCount
 
-			changes.append([sbscpy[lecture]["users"], extractInfos(videos[0])])
+            changes.append([sbscpy[lecture]["users"], extractInfos(videos[0])])
 
-	save()
-	return changes
+    subscriptions = sbscpy
+    save()
+    return changes
